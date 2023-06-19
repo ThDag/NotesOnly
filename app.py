b10a3c81d538e9eb4d0873003357b0b462da0a68
ThDag <tahadoktor@gmail.com>
2023-06-19 10:57:50 +0300

    added add_note feature to typer

--- app.py
+++ app.py
@@ -4,14 +4,30 @@ import csv
 app = typer.Typer()
 
 @app.command()
-def add_note(Text: str='null', Class: int=0):
-    # fill here with using open() and csv module make datas.csv add a new column with note-id, Text, Class
+def add_note(note: str, classid: int=2):
 
-    with open('a', 'datas.csv') as file:
-        pass
-    pass
+    # check if Class got any invalid values
+    if 4 <= classid or classid <= 0:
+        raise ValueError('Classid parameter can only get; 1, 2, 3')
 
-print(something)
+    # get the newest note_id
+    with open('datas.csv', 'r') as file:
+        data = csv.reader(file)
+        last_row = None
+        for row in data:
+            last_row = row
+
+        if last_row:
+            note_id = int(last_row[0]) + 1
+        else:
+            note_id = 0
+
+    # append the note
+    note_data = [note_id, note, classid]
+    with open('datas.csv', 'a') as file:
+        data = csv.writer(file)
+        data.writerow(note_data)
+
 
 if __name__ == "__main__":
     app()
